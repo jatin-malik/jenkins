3524ca6aae615faee9f58ea9350b665015fee192
jatin-malik <malikjatin881@gmail.com>
2022-03-01 11:34:44 +0530

    testing 1

--- test.go
+++ test.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main(){
-  fmt.Println("This is the new test file to see if the jenkins build is working fine and it izz!") 
+  fmt.Println("woah , This is the new test file to see if the jenkins build is working fine and it izz!") 
 }
